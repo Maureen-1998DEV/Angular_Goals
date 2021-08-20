2f3c93f8760d2638565b60af8ac0e20e01c8aa0e
Maureen Ougo <maureen.ougo@student.moringaschool.com>
2021-08-20 03:46:16 +0300

    Adding file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,18 @@ import {Goal} from './goal';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  goals: Goal[]=[
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+    goals: Goal[] = [
+      new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2020,3,14)),
+      new Goal(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2019,6,9)),
+      new Goal(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2022,1,12)),
+      new Goal(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2019,0,18)),
+      new Goal(5,'Solve math homework','Damn Math',new Date(2019,2,14)),
+      new Goal(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2030,3,14)),
+    ];
 
-    {id:1, name:'Watch finding Nemo'},
-    {id:2, name:'Buy Cookies'},
-    {id:3, name:'Get newPhone cases'},
-    {id:4, name:'Get Dog Food'},
-    {id:5, name:'Solve math homework'},
-    // {id:6, name:'plot myworld domination plan'},
-  ];
+
+  
 }
